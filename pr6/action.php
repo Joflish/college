798c4f81399bb14e408a71675c426dfd0ca95d60
Matvei <joflish@yandex.ru>
2023-09-30 10:27:54 +0500

    исправил проверку имени

--- pr6/action.php
+++ pr6/action.php
@@ -12,7 +12,7 @@
     };
 
     if(empty($_POST['name'])) exit('Поле "имя" не заполнено');
-    if (preg_match("/^\d+$/", $_POST['name'])) exit ('Поле "имя" введено не корректно');
+    if (preg_match("/\d+/", $_POST['name'])) exit ('Поле "имя" введено не корректно');
     if((int)$_POST['age'] < 1) exit('Поле "возраст" введено некорректное значение');
     if(empty($_POST['age'])) exit ("Поле 'возраст' не заполнено");
         echo 'Здравствуйте, '.htmlspecialchars($_POST['name']).'. <br>';
