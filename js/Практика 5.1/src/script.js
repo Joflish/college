0731fe3d8ceb0f01c922363b8b8b889700bd32e5
Matvei <joflish@yandex.ru>
2023-10-20 10:11:44 +0500

    5.1

--- js/Практика 5.1/src/script.js
+++ js/Практика 5.1/src/script.js
@@ -1,8 +1,8 @@
 let login = prompt("Введите логин");
 
-if (login = "Админ") {
+if (login === "Админ") {
     let password = prompt("Введите пароль");
-        if (password = "Я главный") {
+        if (password === "Я главный") {
             alert("Здравствуйте")
         } else if (password !== "Я главный") {
             alert("Неверный пароль")
